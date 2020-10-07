451860296b7f3ae6fd61d4b5e80b33d0cfb10130
hsyoon <hsyoon@enliple.com>
2020-10-07 14:05:29 +0900

    중간 커밋3

--- src/main/java/com/example/batch/Controller/KafkaProducerController.java
+++ src/main/java/com/example/batch/Controller/KafkaProducerController.java
@@ -1,23 +1,23 @@
-package com.example.batch.Controller;
-
-import com.example.batch.service.KafkaProducerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(value = "/kafka")
-public class KafkaProducerController {
-
-  private final KafkaProducerService producerService;
-
-  @Autowired
-  public KafkaProducerController(KafkaProducerService producerService)
-  {
-    this.producerService = producerService;
-  }
-
-  @GetMapping(value = "/publish")
-  public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
-    this.producerService.sendMessage(message);
-  }
-}
+//package com.example.batch.Controller;
+//
+//import com.example.batch.service.KafkaProducerService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping(value = "/kafka")
+//public class KafkaProducerController {
+//
+//  private final KafkaProducerService producerService;
+//
+//  @Autowired
+//  public KafkaProducerController(KafkaProducerService producerService)
+//  {
+//    this.producerService = producerService;
+//  }
+//
+//  @GetMapping(value = "/publish")
+//  public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+//    this.producerService.sendMessage(message);
+//  }
+//}
